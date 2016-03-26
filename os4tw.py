be3b32e96802fc5937d3f29b23f935ca2996e532
Mattia Reggiani <info@mattiareggiani.com>
2016-03-26 15:45:05 +0100

    first commit

--- os4tw.py
+++ os4tw.py
@@ -9,9 +9,9 @@ os4tw v.0.1 - OSINT and Digital Investigation toolkit for Twitter
 #                                                                     	#
 # This program is free software: you can redistribute it and/or modify	#
 # it under the terms of the GNU General Public License as published by	#
-# the Free Software Foundation, either version 3 of the License, or		#
-# (at your option) any later version.								   	#
-#																	   	#
+# the Free Software Foundation, either version 3 of the License, or	#
+# (at your option) any later version.					#
+#									#
 # This program is distributed in the hope that it will be useful,      	#
 # but WITHOUT ANY WARRANTY; without even the implied warranty of       	#
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the        	#
